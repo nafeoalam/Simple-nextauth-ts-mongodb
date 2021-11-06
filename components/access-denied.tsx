4f46db6f09d055a01f5434500c2aeedd755d4082
nafeoalam <nafeo.joy@gmail.com>
2021-11-06 23:45:08 +0600

    Some Design Changes

--- components/access-denied.tsx
+++ components/access-denied.tsx
@@ -1,10 +1,11 @@
 import { signIn } from 'next-auth/client'
+import styles from "./access-denied.module.css"
 
 export default function AccessDenied () {
   return (
     <>
-      <h1>Access Denied</h1>
-      <p>
+      <h1 className='font-black'> Access Denied</h1>
+      <p className={styles.link}>
         <a href="/api/auth/signin"
            onClick={(e) => {
            e.preventDefault()
